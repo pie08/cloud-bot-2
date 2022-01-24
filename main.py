0c1cfe7cdaf72679e4777b64403ad2eb87b2e696
pie08 <tyrusbrianberggren@gmail.com>
2022-01-24 16:44:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ import string
 import motor
 from motor import motor_asyncio
 
-print('Hello World')
-
 try:
     cluster = motor_asyncio.AsyncIOMotorClient(
         "mongodb+srv://tyrus:Tyrus113@cluster0.qifea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")  # here will be the link for your cluster
